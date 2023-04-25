40cfcf7b7d274824ce657ca333711018eb660952
rupamurugan <131766274+rupamurugan@users.noreply.github.com>
2023-04-25 11:54:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.title("Briniyani")
 
 streamlit.header("shriyaan is my life")
 streamlit.text("i hate typing")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
